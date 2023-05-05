76386a3dafd673fb89243c124fc87fd0698ba9e8
jppuig <jppuigpesce@gmail.com>
2023-05-05 16:35:23 -0300

    Ejericio1.sh

--- Ejercicio1.sh
+++ Ejercicio1.sh
@@ -6,18 +6,30 @@ DIVISAS="$ENTRADA/divisas.txt"
 CARGA="$SALIDA/carga.txt"
 COMPRA="$SALIDAS/'Compra de divisas.txt'"
 
+COTIZACION=(0 0 0 0 0 0 0 0) #Orden del array: venta compra para las monedas Euro, Dolar, Arg, Real
+
 cargarDivisas() {
 	hoy=$(date +%d/%m/%Y)
 	if  [ -s $DIVISAS ]; then
-		egrep -i "^$hoy-(EUR|ARG|USD|UYU)-[0-9]{9}-[0-9]{9}-[A-Z]*" $DIVISAS > divisasHoy.txt
-		divisasHoy=$(cat divisasHoy.txt) #Toma divisas del dia
+		egrep -i "^$hoy-(EUR|ARG|USD|BRL)-[0-9]{9}-[0-9]{9}-[A-Z]*" $DIVISAS > divisasHoy.txt
 
 		lineasEuro=$(egrep -c "^$hoy-EUR-[0-9]{9}-[0-9]{9}-[A-Z]*" divisasHoy.txt) #Cuenta cuantas cotizaciones hay en el dia para el euro
-		if [ "$lineasEuro" = "0" ]; then #Caso que no haya cotizaciones
+		if [ "$lineasEuro" = "0" ]; then 	#Caso que no haya cotizaciones
 			echo "$hoy Carga de euros no exitosa: no hay informacion de la divisa en el dia" > $CARGA #Sobreescribe el archivo para borrar lo anterior
-		elif [ "$lineasEuro" = "1" ]; then #Caso que hay una sola cotizacion
-			echo "$hoy La carga de euros fue exitosa" > $CARGA
-		else #Caso que haya mas de una cotizacion
+		elif [ "$lineasEuro" = "1" ]; then 	#Caso que hay una sola cotizacion
+			echo "$(cat divisasHoy.txt | cut -d "-" -f 3)"
+			echo "$(cat divisasHoy.txt | cut -d "-" -f 4)"
+
+			COTIZACION[0]=$(cat divisasHoy.txt | cut -d "-" -f 3)	#Guardo la divisa de compra para Euro
+			COTIZACION[1]=$(cat divisasHoy.txt | cut -d "-" -f 4)	#Guardo la divisa de venta para Euro
+			echo "$COTIZACION[0]"	#ESTAN GUARDANDO LO MISMO
+			echo "$COTIZACION[1]"
+			if [ "$COTIZACION[0]" = "0" || "$COTIZACION[1]" = "0" ]; then
+				echo "$hoy Carga de euros no exitosa: divisas incorrectas"
+			else
+				echo "$hoy La carga de euros fue exitosa" > $CARGA
+			fi
+		else	 #Caso que haya mas de una cotizacion
 			echo "$hoy Carga de euros no exitosa: mas de una cotizacion en el dia" > $CARGA
 		fi
 
@@ -26,7 +38,7 @@ cargarDivisas() {
 			echo "$hoy Carga de dolares no exitosa: no hay informacion de la divisa en el dia" >> $CARGA
 		elif [ "$lineasDolar" = "1" ]; then 
 			echo "$hoy La carga de dolares fue exitosa" >> $CARGA
-		else 
+		else
 			echo "$hoy Carga de dolares no exitosa: mas de una cotizacion en el dia" >> $CARGA
 		fi
 
@@ -35,23 +47,25 @@ cargarDivisas() {
 			echo "$hoy Carga de pesos argentinos no exitosa: no hay informacion de la divisa en el dia" >> $CARGA
 		elif [ "$lineasArg" = "1" ]; then
 			echo "$hoy La carga de pesos argentinos fue exitosa" >> $CARGA
-		else 
+		else
 			echo "$hoy Carga de pesos argentinos no exitosa: mas de una cotizacion en el dia" >> $CARGA
 		fi
 
-		lineasUru=$(egrep -c "^$hoy-UYU-[0-9]{9}-[0-9]{9}-[A-Z]*" divisasHoy.txt)
-		if [ "$lineasUru" = "0" ]; then
-			echo "$hoy Carga de pesos uruguayos no exitosa: no hay informacion de la divisa en el dia" >> $CARGA
-		elif [ "$lineasUru" = "1" ]; then
-			echo "$hoy - La carga de pesos uruguayos fue exitosa" >> $CARGA
-		else 
-			echo "$hoy - Carga de pesos uruguayos no exitosa: mas de una cotizacion en el dia" >> $CARGA
+		lineasReal=$(egrep -c "^$hoy-BRL-[0-9]{9}-[0-9]{9}-[A-Z]*" divisasHoy.txt)
+		if [ "$lineasReal" = "0" ]; then
+			echo "$hoy Carga de reales no exitosa: no hay informacion de la divisa en el dia" >> $CARGA
+		elif [ "$lineasReal" = "1" ]; then
+			echo "$hoy - La carga de reales fue exitosa" >> $CARGA
+		else
+			echo "$hoy - Carga de reales no exitosa: mas de una cotizacion en el dia" >> $CARGA
 		fi
 
 		rm divisasHoy.txt
 	else
-		echo "$hoy - No hay archivo de divisas" > $CARGA
+		echo "$hoy - Carga de monedas no exitosa: No hay archivo de divisas" > $CARGA
 	fi
+
+#	menuPrincipal
 }
 
 menuMoneda() {
@@ -64,19 +78,46 @@ menuMoneda() {
 
 comprarDivisas(){ #De donde las divisas
 	hoy=$(date +%d/%m/%Y)
-	menuMoneda()
-	read moneda
+	menuMoneda
+#	read moneda
 	echo "Elija la operacion"
 	echo "1-Compra"
-	echo "2-Venta"
-	read op
+	echo "2-Venta" # SEPARAR para ventaDivisas
+#	read op
 	echo "Ingrese cantidad de la moneda a comprar"
-	read cantMoneda
+#	read cantMoneda
 
-	if [ "$moneda" = "1" &&  ]; then
-		echo "$hoy - compra " >> $COMPRA
+#	if [ "$moneda" = "1" &&  ]; then
+#		echo "$hoy - compra " >> $COMPRA
 
-	egrep -i "^$hoy-(EUR|ARG|USD|UYU)-[0-9]{9}-[0-9]{9}-[A-Z]*" $DIVISAS > divisasHoy.txt
-	divisasHoy=$(cat divisasHoy.txt) #Toma divisas del dia
+#	egrep -i "^$hoy-(EUR|ARG|USD|UYU)-[0-9]{9}-[0-9]{9}-[A-Z]*" $DIVISAS > divisasHoy.txt
+#	divisasHoy=$(cat divisasHoy.txt) #Toma divisas del dia
 
+	menuPrincipal
 }
+
+menuPrincipal() {
+	echo "Elija una operación:"
+	echo "1-cargar divisas"
+	echo "2-comprar divisas"
+	echo "3-venta divisas"
+	echo "0-terminar programa"
+
+	read op
+
+	if [ $op = "1" ]; then
+		cargarDivisas
+	elif [ $op = "2" ]; then
+		comprarDivisas
+	elif [ $op = "3" ]; then
+		ventaDivisas
+	elif [ $op = "0" ]; then
+		exit
+	else
+		echo "opcion ingresada incorrecta"
+		menuPrincipal
+	fi
+}
+
+cargarDivisas
+#menuPrincipal
